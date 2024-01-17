47c4cbb53a8b890ab929546bf61b6550da38c71d
Avinash Sahu <96678635+Avinash9694@users.noreply.github.com>
2024-01-17 22:18:31 +0530

    added logic for fetching each movie

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/movie/:id" element={<Movie />} />
+      <Route path="/movies/:id" element={<Movie />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
